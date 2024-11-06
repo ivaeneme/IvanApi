64ae9bb73c56f1d5a55e043b6d8013b82864822d
Ivan Martinez <127161473+ivaeneme@users.noreply.github.com>
2024-11-06 20:17:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ if __name__=='__main__':
     app.run(debug=True)
 
 postgresql://menu_xgeh_user:PzjvnW4XvKnUebXK5uz6YQgbjTJgMCiW@dpg-cslv9c1u0jms73b9j2c0-a.oregon-postgres.render.com/menu_xgeh
+
+PzjvnW4XvKnUebXK5uz6YQgbjTJgMCiW
